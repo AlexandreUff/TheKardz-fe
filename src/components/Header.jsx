485c92c7856cd474414c13b76febbce92586b7cc
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-06 23:37:06 -0300

    feat: create main icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { IcoMain } from "./Icons";
+
 export default function Header(){
     return <>
     <header>
         <h1>
             <small>THE</small><span>K</span>ARDZ
         </h1>
+        <IcoMain />
     </header>
     </>
 }
