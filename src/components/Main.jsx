9c18e98f1ed63ae9dddba7d2d7ddd7830b43293b
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-07 11:38:41 -0300

    fix: adjust some name

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Main(){
     return <>
         <main>
             <form>
                 <label htmlFor="name">
-                    Digite seu nome:
+                    Digite o nome da sala:
                 </label>
                 <input type="text" id="name" />
-                <input type="submit" value="CRIAR" title="Criar sala" />
+                <input type="submit" value="ENTRAR" title="Criar sala" />
             </form>
+            <div>
+                <Link to="/username">Ou crie uma sala</Link>
+            </div>
         </main>
     </>
 }
