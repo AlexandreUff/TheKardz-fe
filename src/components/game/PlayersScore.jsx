5958676820b9c522fe608d607291f5b08c1896bb
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-12 17:35:13 -0300

    chore: remove unusul code

--- src/components/game/PlayersScore.jsx
+++ src/components/game/PlayersScore.jsx
@@ -1,11 +1,6 @@
 import { IcoPlayer } from "../Icons";
-import { Users } from "../../Utils/Mocks/Users"
 
 export default function PlayersScore(props){
-
-    const players = Users
-    console.log(players)
-
     return (
         <div className="players-score-scope">
             <IcoPlayer />
