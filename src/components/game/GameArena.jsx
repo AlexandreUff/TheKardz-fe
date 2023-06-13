c32921fe87052f847dcdbc03e048880f41f84dfd
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-13 17:05:48 -0300

    chore: add main layout game

--- src/components/game/GameArena.jsx
+++ src/components/game/GameArena.jsx
@@ -3,8 +3,26 @@ import CardToShow from "./CardToShow"
 export default function GameArena(){
     return (
         <main className="game-arena">
-            {/* {"ARENA"} */}
-            <CardToShow />
+            {/* <CardToShow /> */}
+            <div className="player-name">
+                ENEMY
+            </div>
+            <div className="card-list">
+                <CardToShow />
+                <CardToShow />
+                <CardToShow />
+            </div>
+            <div className="table">
+                <CardToShow />
+            </div>
+            <div className="card-list my-cards"> {/* O jogador só entra nesse lado */}
+                <CardToShow />
+                <CardToShow />
+                <CardToShow />
+            </div>
+            <div className="player-name">
+                VOSSÊ
+            </div>
         </main>
     )
 }
