97cd53b183cae03b4a0c0f2e81bae1f7bdfd636b
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-13 15:47:08 -0300

    chore: create toggle aside panel button to show and hide

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -57,3 +57,23 @@ export const IcoPlayer = () => {
   <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
 </svg>
 }
+
+export const IcoVerticalDots = () => {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+  </svg>
+}
+
+export const IcoLeftArrow = () => {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+  <path d="M8.354 1.646a.5.5 0 0 1 0 .708L2.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+  <path d="M12.354 1.646a.5.5 0 0 1 0 .708L6.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+</svg>
+}
+
+export const IcoRightArrow = () => {
+  return <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16">
+  <path d="M3.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L9.293 8 3.646 2.354a.5.5 0 0 1 0-.708z"/>
+  <path d="M7.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L13.293 8 7.646 2.354a.5.5 0 0 1 0-.708z"/>
+</svg>
+}
--- src/components/game/AsidePanel.jsx
+++ src/components/game/AsidePanel.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
+import { IcoLeftArrow, IcoRightArrow } from "../Icons";
+
 export default function AsidePanel(props){
+
+    const [arrowIcon, setArrowIcon] = useState(true);
+
+    const showAsidePanel = () => {
+        const asidePanel = document.querySelector(".aside-panel")
+        const showPanel = document.querySelector(".show-panel")
+        asidePanel.toggleAttribute("show")
+        showPanel.toggleAttribute("show")
+
+        setArrowIcon(!arrowIcon)
+    }
+
     return (
-        <aside className="aside-panel">
-            <h4>
-                Nº DA SALA:
-            </h4>
-            <h3>
-                2831456
-            </h3>
-        </aside>
+        <>
+            <aside className="aside-panel">
+                <h4>
+                    Nº DA SALA:
+                </h4>
+                <h3>
+                    2831456
+                </h3>
+            </aside>
+            <div className="show-panel" onClick={showAsidePanel}>
+                {arrowIcon ? <IcoLeftArrow /> : <IcoRightArrow />}
+            </div>
+        </>
     )
 }
