b532075c53ceb3dd54771cf95543da12a0890055
AlexandreUff <alexandre_magno@id.uff.br>
2023-06-16 18:20:05 -0300

    fix: trying to solve a build bug

--- src/components/game/Timer.jsx
+++ src/components/game/Timer.jsx
@@ -5,15 +5,23 @@ export default function Timer(props){
 
     let [time,setTime] = useState(props.time)
 
-    let regressiveTime
+    /* let regressiveTime */
 
     const colorTimer = props.type === "match" ? "green" : "red"
 
     useEffect(()=>{
-        startTimer()
+        const regressiveTime = setInterval(()=>{
+            setTime(prevTime => {
+                if(prevTime <= 0){
+                    clearInterval(regressiveTime)
+                    return prevTime
+                }
+                return prevTime-1
+            })
+        },1000)
     },[])
 
-    function startTimer(){
+    /* function startTimer(){
         regressiveTime = setInterval(()=>{
             setTime(prevTime => {
                 if(prevTime <= 0){
@@ -23,7 +31,7 @@ export default function Timer(props){
                 return prevTime-1
             })
         },1000)
-    }
+    } */
 
     return (
         <div className="timer" style={{border:`${colorTimer} solid 5px`, color: colorTimer}}>
