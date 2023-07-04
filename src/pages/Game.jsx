2020dc1c64ff079dcc09c2de082107b435b57d11
AlexandreUff <alexandre_magno@id.uff.br>
2023-07-04 15:56:43 -0300

    feat: add socket.io

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,13 +1,36 @@
+import { useEffect } from "react";
+import io from "socket.io-client";
 import AsidePanel from "../components/game/AsidePanel";
 import AsidePlayers from "../components/game/AsidePlayers";
 import GameArena from "../components/game/GameArena";
 
-export default function Game(){
-    return (
-        <div className="game-area">
-            <AsidePlayers/>
-            <GameArena/>
-            <AsidePanel/>
-        </div>
-    )
+export default function Game() {
+  
+  useEffect(() => {
+    console.log("TESTE")
+    const socket = io("http://localhost:3002", { transports: ['websocket'] });
+
+    socket.on("connect", () => {
+      console.log("Conexão estabelecida com o servidor socket.io.");
+
+      // Adicione aqui o código para lidar com os eventos de socket.io
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Desconectado do servidor socket.io.");
+    });
+
+    return () => {
+      // Executa quando o componente é desmontado
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="game-area">
+      <AsidePlayers />
+      <GameArena />
+      <AsidePanel />
+    </div>
+  );
 }
