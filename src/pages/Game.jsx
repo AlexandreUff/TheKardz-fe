3cd9ba2cea3a5e98eca12c3e961aa5caac9c71e1
AlexandreUff <alexandre_magno@id.uff.br>
2023-07-05 14:59:01 -0300

    style: adjust setTmeOut code

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,5 +1,4 @@
 import { useEffect } from "react";
-/* import io from "socket.io-client"; */
 import AsidePanel from "../components/game/AsidePanel";
 import AsidePlayers from "../components/game/AsidePlayers";
 import GameArena from "../components/game/GameArena";
@@ -11,21 +10,14 @@ export default function Game() {
   const socket = startSocketService()
 
   useEffect(() => {
-    /* socket.on("connect", () => {
-      console.log("Conexão estabelecida com o servidor socket.io.");
-
-      // Adicione aqui o código para lidar com os eventos de socket.io
-    }); */
-
-    setTimeout(()=>socket.emit("attack","Atacando"),5000)
+    
+    setTimeout(() => {
+      socket.emit("attack","Atacando")
+    }, 5000)
     
     socket.on("send",(msg)=>{
       console.log(msg)
     })
-
-    /* socket.on("disconnect", () => {
-      console.log("Desconectado do servidor socket.io.");
-    }); */
 
     return () => {
       // Executa quando o componente é desmontado
