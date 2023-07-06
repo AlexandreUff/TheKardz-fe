daa4ba18151e284815f4f10284ea305f0ab78512
AlexandreUff <alexandre_magno@id.uff.br>
2023-07-06 11:39:01 -0300

    feat: create API service

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -1,9 +1,47 @@
 class APIService {
-    static baseURL = ""
+    static baseURL = "http://localhost:3001"
 
-    static async get(){
-        /* fetch */
+    static async get(endPoint){
+        try {
+            const response = await fetch(`${this.baseURL}${endPoint}`, {
+                method: 'GET'
+              })
+                /* .then(response => {
+                  // Manipule a resposta
+                })
+                .catch(error => {
+                  // Lide com erros
+                });*/
+              return response
+
+        } catch (error) {
+            console.log("Erro ao buscar dado(s) do servidor.", error)
+        }
+
     }
+
+    static async post(endPoint, data){
+        try {
+            const response = await fetch(`${this.baseURL}${endPoint}`, {
+              method: 'POST',
+              body: JSON.stringify(data), // Opcional: envie dados no corpo da solicitação
+              headers: {
+                'Content-Type': 'application/json' // Opcional: especifique o tipo de conteúdo do corpo da solicitação
+              }
+            })
+              /* .then(response => {
+                // Manipule a resposta
+              })
+              .catch(error => {
+                // Lide com erros
+              }); */
+              return response
+
+        } catch (error) {
+            console.log("Erro ao enviar dado(s) ao servidor.", error)
+        }
+    }
+
 }
 
 export default APIService
