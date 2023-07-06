eb64df842237c9e89ea2381c3e5a9eb1fb8ff4ae
AlexandreUff <alexandre_magno@id.uff.br>
2023-07-06 19:03:48 -0300

    feat: conclusion of API service with all CRUD methods

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -45,6 +45,47 @@ class APIService {
         }
     }
 
+    static async put(endPoint, data){
+      try {
+        let response
+            await fetch(`${this.baseURL}${endPoint}`, {
+              method: 'PUT',
+              body: JSON.stringify(data),
+              headers: {
+                'Content-Type': 'application/json'
+              }
+            })
+              .then(async response => {
+                return response.json()
+              }).then(data => {
+                response = data
+              })
+              
+            return response
+      } catch (error) {
+        console.log("Erro ao atualizar dado(s) no servidor.", error)
+      }
+    }
+
+    static async delete(endPoint, data){
+      try {
+        let response
+            await fetch(`${this.baseURL}${endPoint}${data}`, {
+                method: 'DELETE'
+              })
+                .then(response => {
+                  return response.json()
+                })
+                .then(data => {
+                  response = data
+                })
+                
+            return response
+      } catch (error) {
+        console.log("Erro ao remover dado(s) do servidor.", error)
+      }
+    }
+
 }
 
 export default APIService
