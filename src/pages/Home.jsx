081a86c72f02187d1e38cebed0745c94214adbab
AlexandreUff <alexandre_magno@id.uff.br>
2023-07-06 23:08:29 -0300

    feat: set a fetch hall number at home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import APIService from "../services/APIService";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [hallNumber,setHallNumber] = useState("")
+  const [warning,setWarning] = useState("")
+
+  const navigate = useNavigate()
+
+  const takeHallNumber = async (e) => {
+    e.preventDefault()
+    const response = await APIService.get(`/hall?number=${hallNumber}`)
+    
+    if(response.status){
+      navigate(`/username/${response.data}`)
+    } else {
+      setWarning(response.message)
+    }
+  }
+
   return (
     <>
       <Header />
       <Main>
         <form>
           <label htmlFor="hall">Digite abaixo o número da sala:</label>
-          <input type="text" id="hall" />
-          <input type="submit" value="ENTRAR" title="Entrar na sala" />
+          <input type="text" id="hall" value={hallNumber} onChange={e => setHallNumber(e.target.value)} />
+          {warning && <p className="warning">
+            {warning}
+          </p>}
+          <input type="submit" value="ENTRAR" title="Entrar na sala" onClick={takeHallNumber} />
         </form>
         <div>
           <Link to="/username/crt">Ou crie uma sala</Link>
