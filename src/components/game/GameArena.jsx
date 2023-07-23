903f3a0074ecf904d0195fb501fd7a426cc02d28
AlexandreUff <alexandre_magno@id.uff.br>
2023-07-23 19:27:24 -0300

    fix: solve undefined _id issue to send movement

--- src/components/game/GameArena.jsx
+++ src/components/game/GameArena.jsx
@@ -66,15 +66,15 @@ export default function GameArena() {
         const onlyUsersId = [playersWillFight[0]._id, playersWillFight[1]._id];
         const index = onlyUsersId.indexOf(userId);
 
-        if (index !== -1 && index !== 1) {
-          const temp = playersWillFight[0];
-          playersWillFight[0] = playersWillFight[1];
-          playersWillFight[1] = temp;
+        if (index !== -1 && index !== 0) {
+          const temp = playersWillFight[1];
+          playersWillFight[1] = playersWillFight[0];
+          playersWillFight[0] = temp;
         }
 
         setPlayersAreFighting([...playersWillFight]);
       } else {
-        setPlayersAreFighting([]);
+        setPlayersAreFighting([...users]);  /* Já pôr o player aguardante aqui */
       }
     });
   }, [/* playersAreFighting,  */socket]);
@@ -85,8 +85,9 @@ export default function GameArena() {
 
 
   const sendStartRoundStatus = () => {
-    if(playersAreFighting[1]._id === userId && playersAreFighting[1].lineNumber === 0){
-      console.log(`${playersAreFighting[1].name} de ${playersAreFighting[1].lineNumber} iniciou a partida.`);
+    console.log("SENDSTARTFUNC",playersAreFighting[0])
+    if(playersAreFighting[0]._id === userId && playersAreFighting[0].lineNumber === 0){
+      console.log(`${playersAreFighting[0].name} de ${playersAreFighting[0].lineNumber} iniciou a partida.`);
       socket.send("starting-round");
     }
   }
@@ -113,7 +114,7 @@ export default function GameArena() {
         player: {
           userName: userName,
           userId: userId,
-          lineNumber: playersAreFighting[1].lineNumber
+          lineNumber: playersAreFighting[0].lineNumber
         },
         movement: {
           ...movementWillBeSent
@@ -142,11 +143,11 @@ export default function GameArena() {
       <div className="player-name">
         <div className="unused-area"></div>
         <h5 className="top">
-          {playersAreFighting.length > 1 && playersAreFighting[0].name}
+          {playersAreFighting.length > 1 && playersAreFighting[1].name}
         </h5>
         <h6>
           {playersAreFighting.length > 1 &&
-            `🏆 ${playersAreFighting[0].victories}  ☠️ ${playersAreFighting[0].loses}`}
+            `🏆 ${playersAreFighting[1].victories}  ☠️ ${playersAreFighting[1].loses}`}
         </h6>
       </div>
       <div className="card-list">
@@ -189,7 +190,7 @@ export default function GameArena() {
         {" "}
         {/* O jogador só entra nesse lado */}
         {/* Verifica se é o você o jogador. Caso não, os dados não podem ser passados no card */}
-        {playersAreFighting.length > 1 && (playersAreFighting[1].name === userName) ?
+        {playersAreFighting.length > 1 && (playersAreFighting[0].name === userName) ?
           cardsOfPlayerII.map((card, i) => {
             return <CardToShow
                       moviment={card.cardName}
@@ -221,12 +222,12 @@ export default function GameArena() {
           }
         </div>
         <h5 className="bottom">
-          {playersAreFighting.length > 1 && (playersAreFighting[1].name === userName ?
-           <>{playersAreFighting[1].name} <br /><small>(Você)</small></> : (playersAreFighting[1].name))}
+          {playersAreFighting.length > 1 && (playersAreFighting[0].name === userName ?
+           <>{playersAreFighting[0].name} <br /><small>(Você)</small></> : (playersAreFighting[0].name))}
         </h5>
         <h6>
           {playersAreFighting.length > 1 &&
-            `🏆 ${playersAreFighting[1].victories}  ☠️ ${playersAreFighting[1].loses}`}
+            `🏆 ${playersAreFighting[0].victories}  ☠️ ${playersAreFighting[0].loses}`}
         </h6>
       </div>
     </main>
