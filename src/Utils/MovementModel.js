2b5bafae6bdf1e9425208acbbcaa8dcac2e2d56d
AlexandreUff <alexandre_magno@id.uff.br>
2023-08-03 18:34:58 -0300

    feat: create a class CardModel

---
+++ src/Utils/MovementModel.js
@@ -0,0 +1,10 @@
+class CardModel {
+    constructor(cardName, amount, type, selected){
+        this.cardName = cardName;
+        this.amount = amount;
+        this.type = type;
+        this.selected = false;
+    }
+}
+
+export default CardModel
