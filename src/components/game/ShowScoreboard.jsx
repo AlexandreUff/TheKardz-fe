b0d55cc11a15e5e588968b32f2c4d6d2d2ef2b9d
AlexandreUff <alexandre_magno@id.uff.br>
2023-08-15 11:25:19 -0300

    feat: add effects to scoreboard results

--- src/components/game/ShowScoreboard.jsx
+++ src/components/game/ShowScoreboard.jsx
@@ -3,13 +3,29 @@ export default function ShowScoreboard(props){
     const userName = props.username
     const status = props.status
 
+    const emojiStatusShow = () => {
+
+        const countEmoji = new Array(20).fill(0).map((_,i) => {
+            return (
+            <div key={i} className="emoji-status" style={{
+                position:"absolute",
+                top: `${i*5+10}%`,
+                left: `${Math.floor(Math.random() * 110)}%`,
+            }}>
+                {status === "perdeu" ? "😥" : (i % 2 === 0 ? "😁" : "🙌")}
+            </div>
+            )
+        })
+
+        return countEmoji
+    }
+
     return (
         <>
-            <div className="scoreboard">
-                {/* <div className="title-container"> */}
+            <div className={`scoreboard ${status}`}>
                     <h1>{userName.toUpperCase()}</h1>
                     <h2>{status.toUpperCase()}</h2>
-                {/* </div> */}
+                    {emojiStatusShow()}
             </div>
         </>
     )
