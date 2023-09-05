ef775f6b28f3dfa328597bdcb90b9fac402b9b9f
AlexandreUff <alexandre_magno@id.uff.br>
2023-09-05 10:40:09 -0300

    feat: add selection card effect

--- src/components/game/CardToShow.jsx
+++ src/components/game/CardToShow.jsx
@@ -28,7 +28,7 @@ export default function CardEmpty(props){
     },[props.show])
 
     return (
-    <div className={`card-game ${props.show && "turn"} ${props.selected && "selected"}`} title={titleCompleted} onClick={() => props.show && props.chooseMov()}>
+    <div className={`card-game ${props.show && "turn"} ${props.show && props.selected && "selected"}`} title={titleCompleted} onClick={() => props.show && props.chooseMov()}>
         {props.amount > 1 && (
             <div className={`show-amount ${props.amount === Infinity && "is-infinity"}`}>
                 {props.amount === Infinity ? "∞" : props.amount}
@@ -50,7 +50,7 @@ export default function CardEmpty(props){
                 <IcoDiamondCard/><IcoDiamondCard/><IcoDiamondCard/>
             </div>
         </div>
-        <div className="frontface" type={`${props.type}`}>
+        <div className={`frontface ${props.show && props.selected && "selected"}`} type={`${props.type}`}>
             <div>
                 {/* Condicional para caso receba alguma informação de movimento SE FOR PLAYER */}
                 {true && takeMovimentInfo(props.moviment, props.type)}
