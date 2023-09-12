162d39c67f48fdcdc7f31b83464224440995304b
AlexandreUff <alexandre_magno@id.uff.br>
2023-09-12 11:31:21 -0300

    feat: adjusts for carousel

--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -18,10 +18,17 @@ import HP13 from "../assets/images/hp-13.png"
 import HP14 from "../assets/images/hp-14.png"
 import HP15 from "../assets/images/hp-15.png"
 import HP16 from "../assets/images/hp-16.png"
+import HP17 from "../assets/images/hp-17.png"
 
 export default function Help(props){
 
     const [count,setCount] = useState(0)
+
+    const specialDisplay = {
+        display:"grid",
+        gridTemplateColumns:"10% 90%",
+        gridTemplateRows:"repeat(3, 33.33%)"
+    }
 
     return (
         <div className="help">
@@ -48,6 +55,17 @@ export default function Help(props){
                         <p>passe para o lado para conhecer as regras e aprender a jogar.</p>
                     </section>
                     <section>
+                        <p>
+                            <span className="emphasis">The Kardz</span> é um jogo de cartas com disputas em turnos onde os
+                            jogadores recebem um conjunto de cartas pré-definidas e confrontam-se entre em uma mesma sala.
+                        </p>
+                        <p>
+                            Existem várias cartas de vários tipos e cada uma delas possuem uma função específica.
+                            confira nos próximos slides o passo-a-passo  parar criar uma sala, entrar em uma e 
+                            entender o funcionamento do jogo.
+                        </p>
+                    </section>
+                    <section>
                         <p>na tela inicial, você pode digitar um número de uma sala existente para entrar nela.</p>
                         <img src={HP00} className="eff" />
                         <p>caso não haja uma sala ou você não tenha o número de nenhuma, pode criar uma.</p>
@@ -62,11 +80,107 @@ export default function Help(props){
                         </p>
                     </section>
                     <section>
-                        <p>Após a escolha do nome de usuário, você será direcionado para esta tela caso só haja você e mais um outro jogador.</p>
+                        <p>
+                            Se você criou uma sala, a tela será exibida dessa forma:
+                        </p>
+                        <img src={HP17} className="eff" width={"400px"} />
+                        <p>
+                            Nesse momento, você copia o número de sala que aparece no canto superior direito e 
+                            envia para outras pessoas para que elas possam entrar em sua sala e jogar.
+                        </p>
+                    </section>
+                    <section>
+                        <p>Se você optou por entrar em uma sala, você será direcionado para esta tela caso só haja você e mais um outro jogador.</p>
                         <img src={HP03} className="eff" width={"300px"} />
                         <p>Caso já hajam mais jogadores, provavelmente aparecerá uma mensagem de "carregando dados..."
                             que brevemente estarão disponíveis pra você.
                             Nos próximos slides, explicaremos o que são cada um desses elementos presentes na tela.
+                        </p>
+                    </section>
+                    <section>
+                        <p>Essa é a sua configuração inicial de cartas quando começa uma partida.</p>
+                        <img src={HP04} className="eff" width={"300px"} />
+                        <p>Repare, também, que você consegue apenas visualizar as suas cartas. Já as do adversário, jamais!</p>
+                        <p>
+                            Sempre que você estiver jogando uma partida, suas cartas aparecerão <span className="emphasis">sempre</span> na
+                            parte inferior da tela.
+                        </p>
+                        <p>No próximo slide será apresentado o que são e para que servem cada uma das cartas do jogo.</p>
+                    </section>
+                    <section style={specialDisplay}>
+                        <img src={HP08} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta comum de ataque:</span> é basicamente, como o próprio nome diz, a carta que
+                            você usa para atacar seu adverśario. você incia o jogo com uma unidade e
+                            consegue derrotar seu adversário se ele utilizar, numa mesma rodada, a carta
+                            de recarga. Se o adverśario também usar uma carta de ataque comum, haverá um empate.
+                            é possível obter várias unidades dessa carta e nos próximos slides você saberá como.
+                        </p>
+
+                        <img src={HP09} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta comum de defesa:</span> é a carta que te permite
+                            se defender das cartas de ataque comum do adverśario. sua grande vantagem é a quantidade
+                            infinita. existem cartas que são mais fortes que essa defesa, mas isso veremos nos pŕoximos slides.
+                        </p>
+                        <img src={HP10} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta comum de recarga:</span> esta é uma carta  que também 
+                            possui quantidade infinita e lhe permite adquirir uma unidade de carta de ataque comum
+                            ao ser usada em uma rodada. o grande perigo desta carta é que ela lhe torna vulnerável
+                            caso o adversário utilize uma carta de ataque.
+                        </p>
+                    </section>
+                    <section style={specialDisplay}>
+                        <img src={HP11} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta bronze de ataque (bonus):</span> esta é uma carta de ataque que lhe
+                            garante qubrar a defesa e o ataque do adversário no caso dele utilizar as do tipo comum. mas se
+                            você e o adversário utilizarem, ao mesmo tempo, carta de ataque bronze, o efeito em ambos
+                            é nulo e, logo, será empate. para adquirir esta carta, basta que utilize quaisquer cartas
+                            de ataque por duas vezes seguidas. só é possível adquirir apenas uma unidade deste tipo.
+                        </p>
+                        <img src={HP12} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta bronze de defesa (bonus):</span> esta carta lhe garante se defender
+                            no caso do adversário atacar com ataque do tipo bronze ou inferior. para adquirir esta carta,
+                            basta utilizar por duas vezes seguidas cartas de defesa de quaisquer tipos, mas esta,
+                            diferentemente de sua versão comum, não possui quantidade infinida e só é possível adquirir
+                            uma unidade deste tipo.
+                        </p>
+                        <img src={HP13} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta bronze de recarga (bonus):</span> esta carta lhe permite
+                            adquirir, em uma única jogada, duas cartas de ataque do tipo comum. apesar de ser do
+                            tipo bronze, ela não é capaz de barrar nem mesmo o ataque de cartas comuns e , assim
+                            como a carta bronze de defesa e de ataque, também não é infinita e só é possível adquirir
+                            uma unidade. para adquirí-la, basta usar duas vezes seguidas a carta de recarga de
+                            qualquer tipo.
+                        </p>
+                    </section>
+                    <section style={specialDisplay}>
+                        <img src={HP14} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta gold de ataque (bonus):</span> é a mais forte de todas as cartas
+                            de ataque, pois possui a capacidade de quebrar as defesas das cartas do tipo bronze e comum.
+                            para adquirí-la, basta utilizar por três vezes seguidas as cartas de ataque de quaisquer tipos.
+                            esta carta, assim como todas de ataque, também possui efeito nulo caso se adversário a utilize ao
+                            numa mesma rodada que você e só é possível adquirir uma unidade.
+                        </p>
+                        <img src={HP15} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta gold de defesa (bonus):</span> esta carta é a única que lhe garante
+                            uma defesa contra a carta de ataque do tipo gold e também, é claro, de tipos inferiores.
+                            para adquirí-la, basta utilizar por três vezes seguidas as cartas de defesa de quaisquer tipo.
+                            este tipo, assim como a do tipo bronze, também não é infinita e só é possível adquirir uma unidade.
+                        </p>
+                        <img src={HP16} className="eff" width={"80px"} />
+                        <p>
+                            <span className="emphasis">carta gold de recarga (bonus):</span> esta carta lhe permite adquirir,
+                            em uma única jogada, três cartas de ataque do tipo comum. ainda assim, ela é uma carta
+                            vulnerável, podendo ser vencida até mesmo por uma carta de ataque do tipo comum. para adquirí-la,
+                            basta utilizar, por três vezes seguidas, cartas de recarga de quaisquer tipos. ela, assim como a de
+                            bronze, não é infinita e só é possível adquirir uma unidade.
                         </p>
                     </section>
                     <section>
@@ -84,106 +198,6 @@ export default function Help(props){
                             Esse é o tempo suficiente para que o jogador escolha umas das cartas
                             disponíveis.
                         </p>
-                    </section>
-                    <section>
-                        <p>Essa é a sua configuração inicial de cartas quando começa uma partida.</p>
-                        <img src={HP04} className="eff" width={"300px"} />
-                        <p>Repare, também, que você consegue apenas visualizar as suas cartas. Já as do adversário, jamais!</p>
-                        <p>No próximo slide será apresentado o que são e para que servem cada uma das cartas do jogo.</p>
-                    </section>
-                    <section>
-                        <img src={HP08} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta comum de ataque:</span> é basicamente, como o próprio nome diz, a carta que
-                            você usa para atacar seu adverśario. você incia o jogo com uma unidade e
-                            consegue derrotar seu adversário se ele utilizar, numa mesma rodada, a carta
-                            de recarga. Se o adverśario também usar uma carta de ataque comum, haverá um empate.
-                        </p>
-                        <br />
-                        <img src={HP09} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta comum de defesa:</span> é a carta que te permite
-                            se defender das cartas de ataque comum do adverśario. sua grande vantagem é a quantidade
-                            infinita. existem cartas que são mais fortes que essa defesa, mas isso veremos nos pŕoximos slides.
-                        </p>
-                    </section>
-                    <section>
-                        <img src={HP10} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta comum de recarga:</span> esta é uma carta  que também 
-                            possui quantidade infinita e lhe permite adquirir uma unidade de carta de ataque comum
-                            ao ser usada em uma rodada. o grande perigo desta carta é que ela lhe torna vulnerável
-                            caso o adversário utilize uma carta de ataque.
-                        </p>
-                        <br />
-                        <img src={HP11} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta bronze de ataque (bonus):</span> esta é uma carta de ataque que lhe
-                            garante qubrar a defesa e o ataque do adversário no caso dele utilizar as do tipo comum. se
-                            você e o adversário utilizarem, ao mesmo tempo, carta de ataque bronze, o efeito em ambos
-                            é nulo e, logo, será empate. para adquirir esta carta, basta que utilize quaisquer cartas
-                            de ataque por duas vezes seguidas.
-                        </p>
-                    </section>
-                    <section>
-                        <img src={HP12} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta bronze de defesa (bonus):</span> esta carta lhe garante se defender
-                            no caso do adversário atacar com ataque do tipo bronze ou inferior. para adquirir esta carta,
-                            basta utilizar por duas vezes seguidas cartas de defesa de quaisquer tipos, mas esta,
-                            diferentemente de sua versão comum, não possui quantidade infinida.
-                        </p>
-                        <br />
-                        <img src={HP13} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta bronze de recarga (bonus):</span> esta carta lhe permite
-                            adquirir, em uma única jogada, duas cartas de ataque do tipo comum. apesar de ser do
-                            tipo bronze, ela não é capaz de barrar nem mesmo o ataque de cartas comuns e , assim
-                            como a carta bronze de defesa, também não é infinita. para adquirí-la, basta usar duas
-                            vezes seguidas a carta de recarga de qualquer tipo.
-                        </p>
-                    </section>
-                    <section>
-                        <img src={HP14} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta gold de ataque (bonus):</span> é a mais forte de todas as cartas
-                            de ataque, pois possui a capacidade de quebrar as defesas das cartas do tipo bronze e comum.
-                            para adquirí-la, basta utilizar por três vezes seguidas as cartas de ataque de quaisquer tipos.
-                            esta carta, assim como todas de ataque, também possui efeito nulo caso se adversário a utilize ao
-                            mesmo tempo que você.
-                        </p>
-                        <br />
-                        <img src={HP15} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta gold de defesa (bonus):</span> esta carta é a única que lhe garante
-                            uma defesa contra a carta de ataque do tipo gold e também, é claro, de tipos inferiores.
-                            para adquirí-la, basta utilizar por três vezes seguidas as cartas de defesa de quaisquer tipo.
-                            este tipo, assim como a do tipo bronze, também não é infinita.
-                        </p>
-                    </section>
-                    <section>
-                        <img src={HP16} className="eff" width={"80px"} />
-                        <p>
-                            <span className="emphasis">carta gold de recarga (bonus):</span> esta carta lhe permite adquirir,
-                            em uma única jogada, três cartas de ataque do tipo comum. ainda assim, ela é uma carta
-                            vulnerável, podendo ser vencida até mesmo por uma carta de ataque do tipo comum. para adquirí-la,
-                            basta utilizar, por três vezes seguidas, cartas de recarga de quaisquer tipos. ela, assim como a de
-                            bronze, não é infinita.
-                        </p>
-                        <br />
-                        <p>
-                            OBSERVAÇÃO!
-                        </p>
-                        <p>
-                            Todas as cartas bonus, como pode ser observado, são adquiridas apenas uma unidade e 
-                            mediante sucessivos usos de uma mesma carta de qualquer tipo, mas ao ser usada
-                            você já não poderá mais a adquirir novamente numa mesma sequência, a não ser que haja 
-                            uma quebra dessa sequência escolhendo uma carta distinta. Após isso, todo o processo
-                            poderá ser repetido e se tornará possível adquirir, novamente, a(s) carta(s) bonus.
-                        </p>
-                    </section>
-                    <section>
-
                     </section>
                 </div>
             </div>
